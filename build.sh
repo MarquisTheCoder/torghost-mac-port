6f4df3c7aea70231b7e31ffd9d5b8a276991a58c
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2022-11-20 21:23:37 -0600

    removing commented out pip installations

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
-echo "Torghost installer v3.0"
+echo "Torghost[MAC PORT] installer v3.0"
 echo "Installing brew prerequisites "
 
-#brew install tor
-#brew install pyinstaller
+brew install tor
+brew install pyinstaller
 
 echo "Installing dependencies"
 
-#sudo pip3 install -r requirements.txt 
+sudo pip3 install -r requirements.txt 
 pyinstaller --onefile torghost.py --clean
 if [ $? -eq 0 ]; then
     echo [SUCCESS] generated torghost binary!
@@ -18,7 +18,7 @@ fi
 echo Moving torghost binary to the /usr/bin directory
 
 cd dist 
-sudo cp  torghost /usr/local/bin
+sudo cp torghost /usr/local/bin
 if [ $? -eq 0 ]; then
     echo [SUCCESS] moved torghost binary to the /usr/bin directory
 else
